835b2aaa81862f9a432062569060b6cbe07be49d
Pedro Silva <pedrosilva@MacBook-Air-de-Pedro.local>
2022-10-08 16:00:56 +0100

    fixed background error

--- ProjectFolder/src/org/academiadecodigo/bootcamp/project/Main.java
+++ ProjectFolder/src/org/academiadecodigo/bootcamp/project/Main.java
@@ -3,7 +3,7 @@ import org.academiadecodigo.simplegraphics.pictures.Picture;
 
 public class Main {
     public static void main(String[] args) {
-        Picture background = new Picture(10, 10, "src/org/academiadecodigo/bootcamp/project/pics/Ativo-3-100.jpeg");
+        Picture background = new Picture(10, 10, "ProjectFolder/src/org/academiadecodigo/bootcamp/project/pics/Ativo-3-100.jpeg");
         background.draw();
     }
 }
