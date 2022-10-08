da823328f87a64f135754ab0a54c4492c328a700
Rodrigo Manuel <r.silvamanuel42@gmail.com>
2022-10-08 15:47:10 +0100

    fail add bg pic

---
+++ ProjectFolder/src/org/academiadecodigo/bootcamp/project/Main.java
@@ -0,0 +1,9 @@
+package org.academiadecodigo.bootcamp.project;
+import org.academiadecodigo.simplegraphics.pictures.Picture;
+
+public class Main {
+    public static void main(String[] args) {
+        Picture background = new Picture(10, 10, "src/org/academiadecodigo/bootcamp/project/pics/Ativo-3-100.jpeg");
+        background.draw();
+    }
+}
