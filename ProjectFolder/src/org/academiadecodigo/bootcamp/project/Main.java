75b18c45af9848aba71882810c8b511a17aaa167
Rodrigo Manuel <r.silvamanuel42@gmail.com>
2022-10-09 19:11:36 +0100

    rodrigo is moving 🥳

--- ProjectFolder/src/org/academiadecodigo/bootcamp/project/Main.java
+++ ProjectFolder/src/org/academiadecodigo/bootcamp/project/Main.java
@@ -1,11 +1,11 @@
 package org.academiadecodigo.bootcamp.project;
 import org.academiadecodigo.bootcamp.project.rodrigo.Rodrigo;
+import org.academiadecodigo.simplegraphics.keyboard.Keyboard;
 import org.academiadecodigo.simplegraphics.pictures.Picture;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        Picture background = new Picture(500, 500);
-        background.load("./pics/sets/base_1920x1080.png");
+    public static void main(String[] args) {
+        Picture background = new Picture(0, 0, "./pics/sets/base.png");
         background.draw();
         Rodrigo rodrigo = new Rodrigo();
         rodrigo.move();
--- ProjectFolder/src/org/academiadecodigo/bootcamp/project/rodrigo/Rodrigo.java
+++ ProjectFolder/src/org/academiadecodigo/bootcamp/project/rodrigo/Rodrigo.java
@@ -1,24 +1,73 @@
 package org.academiadecodigo.bootcamp.project.rodrigo;
 
+import org.academiadecodigo.simplegraphics.keyboard.Keyboard;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEventType;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardHandler;
 import org.academiadecodigo.simplegraphics.pictures.Picture;
 
-public class Rodrigo {
+public class Rodrigo implements KeyboardHandler {
 
-    private String path = "./pics/characters/rodrigoLeft.png";
+    private String pathLeft = "./pics/characters/rodrigoLeft.png";
+
+    private String pathRigth = "./pics/characters/rodrigoRight.png";
     private Picture pic;
 
-    public void move() throws InterruptedException {
+    private Keyboard keyboard;
 
-        while(true) {
-            pic.translate(10, 0);
-            Thread.sleep(1000);
-        }
+    public void move(){
+        pic.translate(10, 0);
     }
 
     public Rodrigo() {
         this.pic = new Picture();
-        pic.load(path);
+        pic.load(pathLeft);
         pic.draw();
-        System.out.println(pic.getWidth() + " " + pic.getHeight());
+        this.keyboard = new Keyboard(this);
+        initKeyboard();
+    }
+
+    private void initKeyboard(){
+        KeyboardEvent right = new KeyboardEvent();
+        right.setKey(KeyboardEvent.KEY_RIGHT);
+        right.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+        KeyboardEvent left = new KeyboardEvent();
+        left.setKey(KeyboardEvent.KEY_LEFT);
+        left.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+        KeyboardEvent down = new KeyboardEvent();
+        down.setKey(KeyboardEvent.KEY_DOWN);
+        down.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+        KeyboardEvent up = new KeyboardEvent();
+        up.setKey(KeyboardEvent.KEY_UP);
+        up.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+        keyboard.addEventListener(right);
+        keyboard.addEventListener(left);
+        keyboard.addEventListener(down);
+        keyboard.addEventListener(up);
+    }
+
+    @Override
+    public void keyPressed(KeyboardEvent keyboardEvent) {
+        move(keyboardEvent);
+    }
+
+    private void move(KeyboardEvent keyboardEvent) {
+        System.out.println(keyboardEvent.getKey());
+        if (keyboardEvent.getKey() == KeyboardEvent.KEY_RIGHT){
+            pic.load(pathRigth);
+            pic.translate(10, 0);
+        } else if (keyboardEvent.getKey() == KeyboardEvent.KEY_LEFT) {
+            pic.load(pathLeft);
+            pic.translate(-10, 0);
+        } else if (keyboardEvent.getKey() == KeyboardEvent.KEY_UP) {
+            pic.translate(0, -10);
+        } else {
+            pic.translate(0, 10);
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyboardEvent keyboardEvent) {
+        move(keyboardEvent);
     }
 }
