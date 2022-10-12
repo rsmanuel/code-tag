4a49b874f463ed85633c31f74b5c96f13ecd2202
Rodrigo Manuel <r.silvamanuel42@gmail.com>
2022-10-12 20:40:10 +0100

    wip

--- ProjectFolder/src/org/academiadecodigo/bootcamp/project/Main.java
+++ ProjectFolder/src/org/academiadecodigo/bootcamp/project/Main.java
@@ -1,25 +1,14 @@
 package org.academiadecodigo.bootcamp.project;
-<<<<<<< HEAD
-import org.academiadecodigo.bootcamp.project.characters.Mike42;
-=======
+
 import org.academiadecodigo.bootcamp.project.characters.Bri;
-import org.academiadecodigo.bootcamp.project.characters.Diogo;
-import org.academiadecodigo.bootcamp.project.characters.Rodrigo;
-import org.academiadecodigo.bootcamp.project.characters.Zuca;
->>>>>>> c97323ff47e564971323115042f129143f0dd327
+import org.academiadecodigo.bootcamp.project.characters.Mike42;
 import org.academiadecodigo.bootcamp.project.map.Base;
 import org.academiadecodigo.bootcamp.project.map.Map;
 
 public class Main {
     public static void main(String[] args) {
-
         Map map = new Base();
-<<<<<<< HEAD
-        //Rodrigo rodrigo = new Rodrigo(map);
         Mike42 mikeCA = new Mike42(map);
-=======
         Bri bri = new Bri(map);
->>>>>>> c97323ff47e564971323115042f129143f0dd327
-
     }
 }
