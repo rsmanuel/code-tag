cc1789c62a017078ab3c515f928605988fe5720e
Rodrigo Manuel <r.silvamanuel42@gmail.com>
2022-10-12 21:46:21 +0100

    delete pics from package folder

--- ProjectFolder/src/org/academiadecodigo/bootcamp/project/map/Map.java
+++ ProjectFolder/src/org/academiadecodigo/bootcamp/project/map/Map.java
@@ -1,9 +1,4 @@
 package org.academiadecodigo.bootcamp.project.map;
-
-<<<<<<< HEAD
-
-=======
->>>>>>> c97323ff47e564971323115042f129143f0dd327
 import org.academiadecodigo.simplegraphics.pictures.Picture;
 
 import java.util.HashMap;
